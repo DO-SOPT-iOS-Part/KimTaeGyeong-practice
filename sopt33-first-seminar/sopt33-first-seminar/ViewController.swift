3bb8019cebec341c153e958c3da1f6d7062f0272
timotheekim10 <timotheekim10@gmail.com>
2023-10-11 14:02:56 +0900

    design: 로그인 뷰 구현

--- sopt33-first-seminar/sopt33-first-seminar/ViewController.swift
+++ sopt33-first-seminar/sopt33-first-seminar/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private var idText: String = ""
+    private var passwordText: String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func idTextFieldDidEditing(_ sender: Any) {
+        guard let textField = sender as? UITextField else {return}
+        if let idText = textField.text {
+            self.idText = idText
+        }
+    }
+    
+    @IBAction func passwordTextFieldDidEditing(_ sender: Any) {
+        guard let textField = sender as? UITextField else {return}
+        if let passwordText = textField.text {
+            self.passwordText = passwordText
+        }
+    }
+    
+    @IBAction func loginButtonTap(_ sender: Any) {
+        print("\(idText)\n\(passwordText)")
+    }
+    
 }
 
