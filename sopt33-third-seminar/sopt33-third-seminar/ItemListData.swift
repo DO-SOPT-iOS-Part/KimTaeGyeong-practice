295eea52b13bbbc386a1b2d0d90409ed5084a4d1
timotheekim10 <timotheekim10@gmail.com>
2023-10-31 02:46:49 +0900

    feat: 표현할 데이터 만들기

---
+++ sopt33-third-seminar/sopt33-third-seminar/ItemListData.swift
@@ -0,0 +1,24 @@
+//
+//  ItemListData.swift
+//  sopt33-third-seminar
+//
+//  Created by 티모시 킴 on 10/31/23.
+//
+
+import Foundation
+
+struct ItemListData {
+    let image: String
+    let item: String
+    let location: String
+    let price: String
+    var isLike: Bool
+    
+    init(image: String, item: String, location: String, price: String, isLike: Bool) {
+        self.image = image
+        self.item = item
+        self.location = location
+        self.price = price
+        self.isLike = isLike
+    }
+}
