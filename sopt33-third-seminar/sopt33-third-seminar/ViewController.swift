2846e5effeb15fed28c2987c7a976754fba11bb2
timotheekim10 <timotheekim10@gmail.com>
2023-10-31 02:28:46 +0900

    feat: UITableView 객체 생성

--- sopt33-third-seminar/sopt33-third-seminar/ViewController.swift
+++ sopt33-third-seminar/sopt33-third-seminar/ViewController.swift
@@ -7,7 +7,19 @@
 
 import UIKit
 
+import SnapKit
+import Then
+
 class ViewController: UIViewController {
+    
+    private let tableView = UITableView(frame: .zero, style: .plain).then {
+        $0.backgroundColor = .init(red: 33.0 / 255.0,
+                                   green: 33.0 / 255.0,
+                                   blue: 33.0 / 255.0,
+                                   alpha: 1.0)
+        $0.separatorColor = .lightGray
+        $0.separatorStyle = .singleLine
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
