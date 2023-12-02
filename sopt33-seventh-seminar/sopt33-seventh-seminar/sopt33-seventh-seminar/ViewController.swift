60fe32f565c68f125dbd3f078695020dce81c9ee
timotheekim10 <timotheekim10@gmail.com>
2023-12-02 14:18:29 +0900

    add: 7차 세미나 프로젝트 추가

---
+++ sopt33-seventh-seminar/sopt33-seventh-seminar/sopt33-seventh-seminar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sopt33-seventh-seminar
+//
+//  Created by 티모시 킴 on 12/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
